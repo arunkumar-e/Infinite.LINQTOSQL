148ef41ed74ef791bc0750e5b32f9516a0e34185
ArunKumar <arunkumar.e@outlook.com>
2023-01-10 17:49:50 +0530

    Add project files.

---
+++ Infinite.LINQTOSQL/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Infinite.LINQTOSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NorthwindClassDataContext context = new NorthwindClassDataContext();
+
+            var products = context.Products.OrderBy(c => c.ProductName).Select(c => c);
+
+            Grid1.DataSource = products.ToList();
+        }
+    }
+}
